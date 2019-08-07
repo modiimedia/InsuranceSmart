a1199db622b9d2c38fee61863bd75c434fdd4dad
Joshmossass <joshmsosso@gmail.com>
2019-08-07 14:52:40 -0500

    add support for checkbox through params + add chec

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -2,7 +2,10 @@ let cfm_container = document.querySelector("#contact-form-modal")
 let cfm_name = document.querySelector("#cfm_name")
 let cfm_email = document.querySelector("#cfm_email")
 let cfm_phone = document.querySelector("#cfm_phone")
-let cfm_zip = document.querySelector("#cfm_zip")
+let cfm_zipcode = document.querySelector("#cfm_zipcode")
+let cfm_birth_month = document.querySelector("#cfm_birth_month")
+let cfm_birth_day = document.querySelector("#cfm_birth_day");
+let cfm_birth_year = document.querySelector("#cfm_birth_year")
 let cfm_closer = document.querySelectorAll(".cfm_closer")
 let html_element = document.querySelector("html");
 
@@ -18,9 +21,18 @@ function checkForParams(){
     let active = window.location.search
     if (active) {
         let params = getUrlParams();
+        console.log(params)
         if (params.modal === "on"){
-            params.name = params.name.replace("+", " ");
-            params.email = params.email.replace("%40", "@");
+            if (params.name) {
+                params.name.replace("+", " ");
+            }
+            if (params.email) {
+                params.email = params.email.replace("%40", "@");
+            }
+            if (params.insurance){
+                let insurance = params.insurance.split(',');
+                params.insurance = insurance
+            }
             openContactFormModal(params)
         }
     }
@@ -38,9 +50,33 @@ function openContactFormModal(object){
     console.log(object)
     html_element.classList.add("no-scroll")
     cfm_container.classList.add("is-open")
-    cfm_name.value = object.name
-    cfm_email.value = object.email
-    cfm_phone.value = object.phone
+    if(object.name){
+        cfm_name.value = object.name        
+    }
+    if(object.email){
+        cfm_email.value = object.email
+    }
+    if(object.phone){
+        cfm_phone.value = object.phone
+    }
+    if(object.zipcode){
+        cfm_zipcode.value = object.zipcode
+    }
+    if(object.insurance){
+        for(let i = 0; i < object.insurance.length; i++){
+            let insuranceType = object.insurance[i];
+            let id = `cfm_check_${insuranceType}_field`
+            if(document.getElementById(id)){
+                checkCheckbox(id)
+            } else {
+                console.log(`element with id "${id}" doesn't exist`)
+            }
+        }
+    }
+}
+
+function checkCheckbox(id){
+    document.getElementById(id).checked = true
 }
 
 function closeContactFormModal(){
