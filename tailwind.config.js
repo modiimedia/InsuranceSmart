6b931199bcc7fb823e45c7e6f0ed47dd279050a8
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2023-01-07 11:48:05 -0600

    add colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./layouts/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: {
+        default: "#ffbc00",
+      },
+      gray: {
+        text: "#333333",
+        900: "#333333",
+      },
+      white: "#ffffff",
+      black: "#000000",
+      current: "current",
+      transparent: "transparent",
+    },
     extend: {},
   },
   plugins: [],
